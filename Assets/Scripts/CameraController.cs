bf840d869e33acfd5a5f8882473861f5a6e0eb48
NU0RI <158851606+NU0RI@users.noreply.github.com>
2024-04-13 22:00:22 +0300

    PlayerController And CameraRotation
    Aga. Teper pers begit begit

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform CameraAxisTransform;
 
-    // Update is called once per frame
+    private float _minAngle = -8;
+    private float _maxAngle = 30;
+    private float _RotationSpeed = 600;
+
     void Update()
     {
-        
+        var newAngleY = transform.localEulerAngles.y + Time.deltaTime * _RotationSpeed * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+
+        var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * _RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+            newAngleX -= 360;
+        newAngleX = Mathf.Clamp(newAngleX, _minAngle, _maxAngle);
+
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _fallVelocity = 0;
+    private CharacterController _characterController;
+
+    private Vector3 _moveVector;
+
+    //public Animator animator;
+    private float gravity = 9.8f;
+    private float jumpForce = 4;
+
+    private float speed = 5;
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * Time.fixedDeltaTime * speed);
+
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+
     void Update()
     {
-        
+        MovmentUpdate();
+        JumpUpdate();
+    }
+
+    private void MovmentUpdate()
+    {
+        _moveVector = Vector3.zero;
+        //var runDirection = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+            //runDirection = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+            //runDirection = 2;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+            //runDirection = 3;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+            //runDirection = 4;
+        }
+
+        //animator.SetInteger("Run Direction", runDirection);
+    }
+    private void JumpUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
     }
 }
